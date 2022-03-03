4d0acb1f735a218c5675292a1d32094da21f30ec
Jorge Saul <jorge.castillo1@correo.uis.edu.co>
2022-03-03 01:04:12 -0500

    Get data array for contracts

--- contracts/pages.py
+++ contracts/pages.py
@@ -21,176 +21,176 @@ class Instrucciones(Page):
 
 class c1(Page):
     form_model = 'player'
-    form_fields = ['c1']
+    form_fields = ['c1', 'd1', 't1']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 1
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c2(Page):
     form_model = 'player'
-    form_fields = ['c2']
-
+    form_fields = ['c2', 'd2', 't2']
+    
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 2
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
     
 
 class c3(Page):
     form_model = 'player'
-    form_fields = ['c3']
+    form_fields = ['c3', 'd3', 't3']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 3
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c4(Page):
     form_model = 'player'
-    form_fields = ['c4']
+    form_fields = ['c4', 'd4', 't4']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 4
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c5(Page):
     form_model = 'player'
-    form_fields = ['c5']
+    form_fields = ['c5', 'd5', 't5']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 5
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c6(Page):
     form_model = 'player'
-    form_fields = ['c6']
+    form_fields = ['c6', 'd6', 't6']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 6
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c7(Page):
     form_model = 'player'
-    form_fields = ['c7']
+    form_fields = ['c7', 'd7', 't7']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 7
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c8(Page):
     form_model = 'player'
-    form_fields = ['c8']
+    form_fields = ['c8', 'd8', 't8']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 8
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c9(Page):
     form_model = 'player'
-    form_fields = ['c9']
+    form_fields = ['c9', 'd9', 't9']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 9
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c10(Page):
     form_model = 'player'
-    form_fields = ['c10']
+    form_fields = ['c10', 'd10', 't10']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 10
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c11(Page):
     form_model = 'player'
-    form_fields = ['c11']
+    form_fields = ['c11', 'd11', 't11']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 11
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c12(Page):
     form_model = 'player'
-    form_fields = ['c12']
+    form_fields = ['c12', 'd12', 't12']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 12
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c13(Page):
     form_model = 'player'
-    form_fields = ['c13']
+    form_fields = ['c13', 'd13', 't13']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 13
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c14(Page):
     form_model = 'player'
-    form_fields = ['c14']
+    form_fields = ['c14', 'd14', 't14']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 14
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c15(Page):
     form_model = 'player'
-    form_fields = ['c15']
+    form_fields = ['c15', 'd15', 't15']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 15
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0]) 
 
 
 class c16(Page):
     form_model = 'player'
-    form_fields = ['c16']
+    form_fields = ['c16', 'd16', 't16']
     
     def is_displayed(self):
         return json.loads(self.participant.vars['orden_preguntas'])[self.round_number-1] == 16
-
+    
     def before_next_page(self):
         self.player.rellenar_campos(self.form_fields[0])
 
@@ -217,7 +217,7 @@ class screen2(Page):
 
 class screen3(Page):
     def vars_for_template(self):
-        index_config = int(self.player.n_contract)
+        index_config = int(self.subsession.round_number - 1)
         config_screen = Constants.config_screens_c[index_config]
         return {
             "config_screen": config_screen
@@ -230,7 +230,7 @@ class screen5(Page):
 
 class screen6(Page):
     def vars_for_template(self):
-        index_config = int(self.player.n_contract)
+        index_config = int(self.subsession.round_number - 1)
         config_screen = Constants.config_screens_c[index_config]
         return {
             "config_screen": config_screen
@@ -239,14 +239,11 @@ class screen6(Page):
 
 class screen7(Page):
     def vars_for_template(self):
-        index_config = int(self.player.n_contract)
+        index_config = int(self.subsession.round_number - 1)
         config_screen = Constants.config_screens_c[index_config]
         return {
             "config_screen": config_screen
         }
-
-    def before_next_page(self):
-        self.player.n_contract += 1
 
 
 page_sequence = [screen1, screen2, screen3]
---
+++ contracts/static/js/contracts.js
@@ -0,0 +1,37 @@
+// JAVASCRIPT FOR CONTRACTS
+
+function next_screen(){
+    formulario = document.getElementById("form");
+    formulario.submit();
+}
+
+function countdown(){
+    b_countdown = document.getElementById("countdown");
+    div_counter = document.getElementById("counter-time");
+    time += 1;
+    b_countdown.innerHTML = 10 - time;
+    if(time >= 6){
+        div_counter.style.backgroundColor = "#ffff66";
+    }
+    if(time >= 10){
+        div_counter.style.backgroundColor = "#ff6666";
+        document.getElementById('controlClick').value += "]"
+        next_screen();
+    }
+}
+
+$(document).ready(function () {
+    $('.form-check-input').click(
+        function () {
+        	dateClick = Date.now();
+        	var timeClick = (dateClick - dateInit)/1000;
+        	inputTimeClick = document.getElementById('timeClick');
+        	inputTimeClick.value = timeClick;
+
+            var valor = $('input:radio:checked').val();
+            inputControlClick = document.getElementById('controlClick');
+            var vi = inputControlClick.value;
+            inputControlClick.value = vi + " " + valor;
+        }
+    );
+});
